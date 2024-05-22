5160f6626228df13a2201c05a7466a0aab0801df
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2024-05-22 23:11:58 +0530

    Add NavBar component to Home page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import { HeroSection } from "./components/HeroSection";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+      <NavBar />
       <div className="container mx-auto mt-24 px-12 py-4">
         <HeroSection />
       </div>
